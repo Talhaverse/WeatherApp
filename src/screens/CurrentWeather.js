a5645da344d027c8cda697381506ea535310cf9c
TalhaAhmed36 <stalha247@gmail.com>
2023-05-21 15:29:03 +0500

    making new components and refactoring old ones

--- src/screens/CurrentWeather.js
+++ src/screens/CurrentWeather.js
@@ -1,81 +1,82 @@
 import React from "react";
-import { View,Text,SafeAreaView,StyleSheet } from "react-native";
-import { Feather } from '@expo/vector-icons';
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import RowText from "../components/RowText";
 
-const CurrentWeather = ( ) => {
-return (
-  <SafeAreaView style={styles.wrapper}>
+const CurrentWeather = () => {
+  const {container,wrapper,temp,feels,highLow,highLowWrapper,bodyWrapper,description,message} = styles
+  return (
+    <SafeAreaView style={wrapper}>
+      <View style={container}>
+        <Feather name="sun" size={100} color="black" />
+        <Text>Current Weather</Text>
+        <Text style={temp}>6</Text>
+        <Text style={feels}>Feels like 5</Text>
 
-  <View style={styles.container}>
-  <Feather name="sun" size={100} color="black" />
-    <Text>Current Weather</Text>
-    <Text style={styles.temp}>6</Text>
-    <Text style={styles.feels}>Feels like 5</Text>
-
-    <View style={styles.highLowWrapper}>
-
-    <Text style={styles.highLow}>High:8</Text>
-    <Text style={styles.highLow}>Low:6</Text>
-
-    </View>
-    </View>
-
-    <View style = {styles.bodyWrapper}>
-
-      <Text style={styles.description}>Its Sunny</Text>
-      <Text style={styles.message}> Perfect Tshirt Weather</Text>
-
-    </View>
+        <RowText
+          messageOne={"High:8"}
+          messageTwo={"Low:6"}
+          containerStyles={highLowWrapper}
+          messageOneStyles={highLow}
+          messageTwoStyles={highLow}
+        />
+      </View>
 
 
-  </SafeAreaView>
-)
-}
+      <RowText
+          messageOne={"Its Sunny"}
+          messageTwo={"Perfect T-Shirt Weather"}
+          containerStyles={bodyWrapper}
+          messageOneStyles={description}
+          messageTwoStyles={message} />
+
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor:'pink',
-    flex:1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
 
-  wrapper : {
+  wrapper: {
     flex: 1,
-    backgroundColor:'pink',
+    backgroundColor: "pink",
   },
   temp: {
-    color:'black',
-    fontSize:'48'
+    color: "black",
+    fontSize: "48",
   },
-  feels : {
-    fontSize:'30',
-    color:'black'
-  },
-
-  highLow : {
-    fontSize:'20',
-    color:'black'
+  feels: {
+    fontSize: "30",
+    color: "black",
   },
 
-  highLowWrapper:{
-    flexDirection:'row',
+  highLow: {
+    fontSize: "20",
+    color: "black",
+  },
+
+  highLowWrapper: {
+    flexDirection: "row",
   },
 
   bodyWrapper: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
-    paddingLeft:25,
-    marginBottom:40
+    justifyContent: "flex-end",
+    alignItems: "flex-start",
+    paddingLeft: 25,
+    marginBottom: 40,
   },
 
   description: {
-    fontSize:'48',
+    fontSize: "48",
   },
-  message:{
-    fontSize:'30'
+  message: {
+    fontSize: "30",
   },
-
-})
+});
 
 export default CurrentWeather;
